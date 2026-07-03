933af2edfad81038e2895fc7993259c91470e9f1
Nazareno Bucciarelli <bnazareno03@gmail.com>
2026-07-03 17:10:48 -0300

    fix race condition by adding `exists` to query

--- packages/models/src/models/Rooms.ts
+++ packages/models/src/models/Rooms.ts
@@ -1218,6 +1218,9 @@ export class RoomsRaw extends BaseRaw<IRoom> implements IRoomsModel {
 	setE2eKeyId(_id: IRoom['_id'], e2eKeyId: IRoom['e2eKeyId'], options: UpdateOptions = {}): Promise<UpdateResult> {
 		const query: Filter<IRoom> = {
 			_id,
+			e2eKeyId: {
+				$exists: false,
+			},
 		};
 
 		const update: UpdateFilter<IRoom> = {
